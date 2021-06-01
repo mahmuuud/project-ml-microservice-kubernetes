61b728e24e4d32e4ac3b100968039486ec050bcc
Mahmoud Muhammed <Mahmoud3341@gmail.com>
2021-06-01 23:47:36 +0200

    Add submission files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mahmuuud/images:sklearn
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sklearn\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=sklearn
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward sklearn 8000:80
